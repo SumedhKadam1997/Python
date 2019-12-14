ec29f88b32f9d6a4f51ded4d718afabbdc8dd8b8
Sumedh Kadam <54591733+UxoriousGhost@users.noreply.github.com>
2019-12-14 19:49:54 +0530

    Add files via upload

---
+++ name2.py
@@ -0,0 +1,6 @@
+s='sumedh'
+
+count=1
+for i in s:
+	print(i*count)
+	count+=1
---
+++ name3.py
@@ -0,0 +1,8 @@
+
+s='sumedh'
+a=1
+for i in s:
+    print(s[0:a])
+    print(' '*a+s[a::])
+    print('-'*len(s))
+    a+=1
