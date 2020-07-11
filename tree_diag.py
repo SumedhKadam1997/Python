d48020a7405ad16be164799fef2f089173211624
Sumedh Kadam <54591733+SumedhKadam1997@users.noreply.github.com>
2020-07-11 13:23:37 +0530

    Add files via upload

---
+++ tree_diag.py
@@ -0,0 +1,10 @@
+import os
+
+os.chdir("C:\\Users\\uxoriousghost\\Desktop")
+
+for root, dirs, files in os.walk(".", topdown=True):
+	
+	for name in files:
+		print(os.path.join(root,name))
+	for name in dirs:
+		print(os.path.join(root,name))
